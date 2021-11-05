44d0e1d02d712574fd2ec86b400f22e755fdc1e0
JanetMutua <janetmutua04@gmail.com>
2021-11-05 15:23:25 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function getCount(){
+    let count = prompt('Enter the number of students in attendance:');
+    document.getElementById('counter-el').innerHTML = count;
+    document.getElementById('studCount').innerText= count;
+}
+
+
